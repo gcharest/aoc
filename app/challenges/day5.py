19087a4601eaa694da08e617e461262cb0ccfac8
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2022-12-15 03:34:09 +0000

    add get_stacks

--- app/challenges/day5.py
+++ app/challenges/day5.py
@@ -1,10 +1,21 @@
+import re
+
+
 class Dock:
     def __init__(self, lines):
+        get_integers = lambda x: re.findall(r"\d+", x)
         self.rows = lines
         self.number_of_rows = len(lines)
+        self.stacks_indexes = get_integers(self.rows[self.number_of_rows - 1])
+
+    def get_stacks(self):
+        return self.rows[:self.number_of_rows - 1]
 
 
 class Move:
+    """Takes a string, returns the quantity of blocks to move,
+    which stack the moves start and which stack it ends."""
+
     def __init__(self, text):
         self.quantity = int(text.split(" ")[1])
         self.start = int(text.split(" ")[3])
@@ -12,6 +23,8 @@ class Move:
 
 
 def puzzle():
+    """Prints the results of the puzzle"""
+
     f = [
         "    [D]    ",
         "[N] [C]    ",
@@ -29,10 +42,10 @@ def puzzle():
     stacks_lines = []
     move_lines = []
     for line in f:
-        if line.find('move') != -1:
+        if line.find("move") != -1:
             move_lines.append(Move(line))
         elif line != "":
             stacks_lines.append(line)
 
     stacks = Dock(stacks_lines)
-    print(stacks.rows)
+    print(stacks.get_stacks())
