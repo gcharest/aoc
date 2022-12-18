5dd5fd180c4833e4f622ac5084520af5f0271374
Guillaume Charest <1690085+gcharest@users.noreply.github.com>
2022-12-18 16:10:04 +0000

    Adjust function for part 2

--- app/challenges/day6.py
+++ app/challenges/day6.py
@@ -1,21 +1,20 @@
-def is_unique_string(sub):
+def is_unique_string(sub, sub_len):
     is_unique = True
-    for i in range(4):
+    for i in range(sub_len):
         if sub.count(sub[i]) > 1:
             is_unique = False
     return is_unique
 
 
-def find_marker(packet):
-    position = 4
-    for i in range(len(packet) - 4):
-        marker = packet[position - 4 : position]
-        if is_unique_string(marker) == True:
+def find_marker(packet, sub_len):
+    position = sub_len
+    for i in range(len(packet) - sub_len):
+        marker = packet[position - sub_len : position]
+        if is_unique_string(marker, sub_len) == True:
             return(position, marker)
         position += 1
     return (position, marker)
-
-
+ 
 
 def puzzle():
     f = open("./challenges/day6_input1.txt", "r", newline=None)
@@ -28,6 +27,11 @@ def puzzle():
     # ]
 
     packets =[] 
+    messages = []
     for packet in f:
-        packets.append(find_marker(packet))
+        packets.append(find_marker(packet, 4))
+        messages.append(find_marker(packet, 14))
+
     print(f"Part 1: {packets[0]}")
+    print(f"Part 2: {messages[0]}")
+    
